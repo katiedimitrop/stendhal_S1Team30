d9b04d307db479f66e883295665aba9451f0702f
George Clarke <george.clarke@student.manchester.ac.uk>
2018-11-06 12:14:25 +0000

    Register Class
    I registered the invisibility ring into both the EntityMap.java file and
    the EntityViewFactory.java file.

--- src/games/stendhal/client/gui/j2d/entity/EntityViewFactory.java
+++ src/games/stendhal/client/gui/j2d/entity/EntityViewFactory.java
@@ -202,6 +202,7 @@ public class EntityViewFactory {
 
 		register("item", "ring", null, Ring2DView.class);
 		register("item", "ring", "wedding", UseableRing2DView.class);
+		register("item", "ring", "invisibility", UseableRing2DView.class);
 
 		register("useable_entity", null, null, UseableEntity2DView.class);
 
