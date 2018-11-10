d68c827b7a7e9ecd4d302326769f19317feda03a
katdm <catherine.dimitropoulaki@student.manchester.ac.uk>
2018-11-10 16:07:13 +0000

    Added teleport scroll for Wofol City
    The lack of a quick way to get to Wofol City was an issue in the game.
    It is now available at the magician house in Ados and has been tested on
    it's ability to be used by the player and on returning the right
    description.

--- tests/games/stendhal/server/entity/item/WofolCityScrollTest.java
+++ tests/games/stendhal/server/entity/item/WofolCityScrollTest.java
@@ -1,32 +1,26 @@
 package games.stendhal.server.entity.item;
 
-import static org.junit.Assert.assertTrue;
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertThat;
-//import static org.junit.Assert.fail;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
-//import java.util.Arrays;
+
 import java.util.HashMap;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-//import games.stendhal.common.constants.Actions;
-//import games.stendhal.server.actions.UseAction;
-//import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
-//import games.stendhal.server.entity.item.scroll.MarkedScroll;
-import games.stendhal.server.entity.item.scroll.WofolCityScroll;
-//import games.stendhal.server.entity.item.scroll.WofolCityScroll;
+import games.stendhal.server.entity.item.scroll.MarkedScroll;
 import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.maps.MockStendlRPWorld;
 import marauroa.common.Log4J;
-//import marauroa.common.game.RPAction;
 import utilities.PlayerTestHelper;
 import utilities.RPClass.ItemTestHelper;
 
+/*
+ * @author Katie Dimitropoulaki
+ */
 public class WofolCityScrollTest
 {
 	//private static final String ZONE_NAME = "ITEMTESTZONE";
@@ -36,6 +30,7 @@ public class WofolCityScrollTest
 		Log4J.init();
 		MockStendlRPWorld.get();
 		ItemTestHelper.generateRPClasses();
+		MockStendlRPWorld.get().addRPZone(new StendhalRPZone("-1_semos_mine_nw", 100, 100));
 
 	}
    @Test
@@ -43,40 +38,27 @@ public class WofolCityScrollTest
    {
 	   MockStendlRPWorld.get();
 	   final StendhalRPZone zone = new StendhalRPZone("TestZone");
-	  
-	   int otherSideOfScrollX = 90;
- 	   int otherSideOfScrollY = 90;
-	   final String destZone = "-1_semos_mine_nw";
 	   final Player player  =  PlayerTestHelper.createPlayer("wcbob");
-	   //PlayerTestHelper.registerPlayer(player,zone.getName());
+	   PlayerTestHelper.registerPlayer(player, "-1_semos_mine_nw");
 	   zone.add(player);
+       player.setPosition(4,6);
+	   final MarkedScroll wofolScroll =  (MarkedScroll) SingletonRepository.getEntityManager().getItem("wofol city scroll");
+       wofolScroll.setInfoString(wofolScroll.getInfoString());
+       zone.add(wofolScroll, false);
+       wofolScroll.setPosition(4,5);
 	   
-	   //Give the player a scroll to Wofol city(in their bag). name,class,subclass,map
-	   final WofolCityScroll wofolScroll = new WofolCityScroll("wofol city scroll","","", new HashMap<String,String>());
-	   player.equip("bag", wofolScroll);
-	   
-	   assertTrue(player.isEquipped("wofol city scroll"));
-	   ///Execute the test
 	   wofolScroll.callUseScroll(player);
 	   
-	   //How to get right zone name returned??
-	   assertThat((player.getZone().getName()),is(destZone));
-	   
-	   //Check that player is at specific coordinates and in wofol city
-	   assertThat(player.getX(),is(otherSideOfScrollX));
-	   assertThat(player.getY(),is(otherSideOfScrollY));
-	   
-	   
-	   ///Tidy up
-	   
-	 
+	   //Compare player position with Infostring of scroll, which contains it's destination zone, and destination coordinates 
+	   assertThat(player.getZone().getName()+" "+player.getX() +" "+player.getY(),is(wofolScroll.getInfoString()));
    }
    
    @Test
    public void testDescribe()
    {   
-	   final WofolCityScroll wofolScroll = new WofolCityScroll("wofol city scroll","","", new HashMap<String,String>());
-	   assertThat(wofolScroll.describe(), is("You see a §'wofol city scroll'. Use it to teleport to Wofol City."));
+	   final MarkedScroll wofolScroll = new MarkedScroll("wofol city scroll","","", new HashMap<String,String>());
+	   wofolScroll.setInfoString(wofolScroll.getInfoString());
+	   assertThat(wofolScroll.describe(), is("You see a §'wofol city scroll'."));
 
    }
 }
