5756fe4e91c78c797c9c40e3b10085196378a55c
Mohammad Jahanzaib Alam <mohammad.alam-2@student.manchester.ac.uk>
2018-11-13 12:16:17 +0000

    Tests written for NPC wheelbarrow seller. Hadvar NPC created and added.

---
+++ src/games/stendhal/server/maps/fado/city/WheelbarrowSellerNPC.java
@@ -0,0 +1,95 @@
+package games.stendhal.server.maps.fado.city;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import games.stendhal.common.grammar.ItemParserResult;
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.rp.StendhalRPAction;
+import games.stendhal.server.entity.mapstuff.block.Wheelbarrow;
+import games.stendhal.server.entity.npc.EventRaiser;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.behaviour.adder.SellerAdder;
+import games.stendhal.server.entity.npc.behaviour.impl.SellerBehaviour;
+import games.stendhal.server.entity.player.Player;
+
+public class WheelbarrowSellerNPC implements ZoneConfigurator {
+
+	public static final int BUYING_PRICE = 100;
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	@Override
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		buildFadoCityArea(zone);
+	}
+
+	private void buildFadoCityArea(final StendhalRPZone zone) {
+		final SpeakerNPC npc = new SpeakerNPC("Hadvar") {
+			@Override
+			protected void createPath() {
+				setPath(null);
+			}
+			
+			@Override
+			protected void createDialog() {
+				class WheelbarrowSellerBehaviour extends SellerBehaviour {
+					WheelbarrowSellerBehaviour(final Map<String, Integer> items) {
+						super(items);
+					}
+
+					@Override
+					public boolean transactAgreedDeal(ItemParserResult res, final EventRaiser seller, final Player player) {
+						if (res.getAmount() > 1) {
+							seller.say("Sorry! I can only sell one wheelbarrow.");
+							return false;
+						} else if (!spawnIsBlocked()) {
+							if (!player.drop("money", getCharge(res, player))) {
+								seller.say("You don't seem to have enough money.");
+								return false;
+							}
+							seller.say("Here you go, your own wheelbarrow! Keep it safe.");
+
+							final Wheelbarrow wheelbarrow = new Wheelbarrow(true);
+							StendhalRPAction.placeat(seller.getZone(), wheelbarrow, seller.getX(), seller.getY() + 1);
+
+							player.notifyWorldAboutChanges();
+
+							return true;
+						} else {
+							say("I need space to give you your wheelbarrow, please move whatever is blocking my space.");
+							return false;
+						}
+					}
+				}
+
+				final Map<String, Integer> items = new HashMap<String, Integer>();
+				items.put("wheelbarrows", BUYING_PRICE);
+
+				addGreeting();
+				addJob("I work as a wheelbarrow seller.");
+				addHelp("I sell wheelbarrows. To buy one, just tell me you want to #buy #wheelbarrow.");
+				addGoodbye();
+				new SellerAdder().addSeller(this, new WheelbarrowSellerBehaviour(items));
+			}
+		};
+
+		npc.setEntityClass("sellernpc");
+		npc.setDescription("Hadvar stands awaiting buyers to purchase his wheelbarrows.");
+		npc.setPosition(36, 44);
+		npc.initHP(100);
+		npc.setSounds(Arrays.asList("cough-11", "cough-2", "cough-3"));
+		zone.add(npc);
+	}
+
+	public static boolean spawnIsBlocked() {
+		return false; // isObstacle(36, 45); TODO: Implement this.
+	}
+
+}
---
+++ tests/games/stendhal/server/maps/fado/city/WheelbarrowSellerNPCTest.java
@@ -0,0 +1,127 @@
+package games.stendhal.server.maps.fado.city;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static utilities.SpeakerNPCTestHelper.getReply;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.fsm.Engine;
+import utilities.QuestHelper;
+import utilities.ZonePlayerAndNPCTestImpl;
+
+/**
+ * Test buying wheelbarrows.
+ *
+ * @author Mohammad Jahanzaib Alam
+ */
+public class WheelbarrowSellerNPCTest extends ZonePlayerAndNPCTestImpl {
+
+	private static final String ZONE_NAME = "0_fado_city";
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		QuestHelper.setUpBeforeClass();
+
+		setupZone(ZONE_NAME);
+	}
+
+	public WheelbarrowSellerNPCTest() {
+		setNpcNames("Hadvar");
+		setZoneForPlayer(ZONE_NAME);
+		addZoneConfigurator(new WheelbarrowSellerNPC(), ZONE_NAME);
+	}
+
+	/**
+	 * Tests for hiAndBye.
+	 */
+	@Test
+	public void testHiAndBye() {
+		final SpeakerNPC npc = getNPC("Hadvar");
+		final Engine en = npc.getEngine();
+
+		assertTrue(en.step(player, "hello"));
+		assertEquals("Greetings! How may I help you?", getReply(npc));
+
+		assertTrue(en.step(player, "bye"));
+		assertEquals("Bye.", getReply(npc));
+	}
+
+	/**
+	 * Tests for buyWheelbarrow.
+	 */
+	@Test
+	public void testBuyWheelbarrow() {
+		final SpeakerNPC npc = getNPC("Hadvar");
+		final Engine en = npc.getEngine();
+
+		assertTrue(en.step(player, "hi"));
+		assertEquals("Greetings! How may I help you?", getReply(npc));
+
+		assertTrue(en.step(player, "job"));
+		assertEquals("I work as a wheelbarrow seller.", getReply(npc));
+
+		assertTrue(en.step(player, "offer"));
+		assertEquals("I sell wheelbarrows.", getReply(npc));
+
+		assertTrue(en.step(player, "buy"));
+		assertEquals("A wheelbarrow will cost 100. Do you want to buy it?", getReply(npc));
+		assertTrue(en.step(player, "no"));
+		assertEquals("Ok, how else may I help you?", getReply(npc));
+
+		assertTrue(en.step(player, "buy dog"));
+		assertEquals("Sorry, I don't sell dogs.", getReply(npc));
+
+		assertTrue(en.step(player, "buy house"));
+		assertEquals("Sorry, I don't sell houses.", getReply(npc));
+
+		assertTrue(en.step(player, "buy someunknownthing"));
+		assertEquals("Sorry, I don't sell someunknownthings.", getReply(npc));
+
+		assertTrue(en.step(player, "buy wheelbarrow"));
+		assertEquals("A wheelbarrow will cost 100. Do you want to buy it?", getReply(npc));
+
+		assertTrue(en.step(player, "no"));
+		assertEquals("Ok, how else may I help you?", getReply(npc));
+
+		assertTrue(en.step(player, "buy wheelbarrow"));
+		assertEquals("A wheelbarrow will cost 100. Do you want to buy it?", getReply(npc));
+
+		assertTrue(en.step(player, "yes"));
+		assertEquals("You don't seem to have enough money.", getReply(npc));
+
+		// equip with enough money to buy one wheelbarrow
+		assertTrue(equipWithMoney(player, 100));
+
+		assertTrue(en.step(player, "buy 2 wheelbarrows"));
+		assertEquals("2 wheelbarrows will cost 200. Do you want to buy them?", getReply(npc));
+
+		assertTrue(en.step(player, "yes"));
+		assertEquals("Sorry! I can only sell one wheelbarrow.", getReply(npc));
+
+		assertTrue(en.step(player, "buy wheelbarrow"));
+		assertEquals("A wheelbarrow will cost 100. Do you want to buy it?", getReply(npc));
+		// Make sure there is no block currently at the position where the wheelbarrow will be spawned.
+		assertFalse(WheelbarrowSellerNPC.spawnIsBlocked());
+
+		assertTrue(en.step(player, "yes"));
+		
+		// Make sure the wheelbarrow has spawned and therefore the spawn location is now blocked.
+		assertTrue(WheelbarrowSellerNPC.spawnIsBlocked());
+		assertEquals("Here you go, your own wheelbarrow! Keep it safe.", getReply(npc));
+
+		// Test for when the spawn location is blocked causing the NPC to say 
+		// something is blocking it and needs to be moved.
+		assertTrue(en.step(player, "offer"));
+		assertTrue(en.step(player, "buy wheelbarrow"));
+		assertTrue(en.step(player, "yes"));
+		
+		assertEquals("I need space to give you your wheelbarrow, please move whatever is blocking my space.", getReply(npc));
+
+		
+	}
+
+}
