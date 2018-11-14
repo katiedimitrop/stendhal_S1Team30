bec4e353da847b39cffc0a55845dbb12260d94cd
luis zurita <intozlz5@student.manchester.ac.uk>
2018-11-14 17:48:25 +0000

    first test probably wrong

---
+++ tests/games/stendhal/server/entity/mapstuff/block/WheelbarrowTest.java
@@ -0,0 +1,70 @@
+package games.stendhal.server.entity.mapstuff.block;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import games.stendhal.common.Direction;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.maps.MockStendlRPWorld;
+import utilities.PlayerTestHelper;
+import utilities.RPClass.BlockTestHelper;
+
+
+/**
+ * 
+ * Test to check if carts dont't refresh when player 
+ * leaves zone, and move away from edge of zone if 
+ * player enters zone at the positionthe cart is in
+ *  
+ */
+
+public class WheelbarrowTest {
+	
+	@BeforeClass
+	public static void beforeClass() {
+		BlockTestHelper.generateRPClasses();
+		PlayerTestHelper.generatePlayerRPClasses();
+        MockStendlRPWorld.get();
+	} //beforeClass 
+	
+	@Test
+	public final void testReset() {
+		Wheelbarrow b = new Wheelbarrow(true);
+		b.setPosition(5, 5);
+		b.setResetBlock(false);
+		StendhalRPZone z = new StendhalRPZone("test", 10, 10);
+		z.add(b);
+		Player player = PlayerTestHelper.createPlayer("testPlayer");
+		b.push(player, Direction.RIGHT);
+		assertThat(Integer.valueOf(b.getX()), is(Integer.valueOf(6)));
+		assertThat(Integer.valueOf(b.getY()), is(Integer.valueOf(5)));
+		b.onExited(player, z);
+		assertThat(Integer.valueOf(b.getX()), is(Integer.valueOf(6)));
+		assertThat(Integer.valueOf(b.getY()), is(Integer.valueOf(5)));
+		
+	} //testReset
+	
+	@Test
+	public final void testWheelbarrowEdge() {
+		Wheelbarrow cart = new Wheelbarrow(true);
+		cart.setPosition(0, 5);
+		StendhalRPZone testzone = new StendhalRPZone("test", 10, 10);
+		testzone.add(cart);
+		Player p = PlayerTestHelper.createPlayer("pusher");
+		assertEquals(0, cart.getX());
+		assertEquals(5, cart.getY());
+		testzone.setEntryPoint(0, 5);;
+		p.setDirection(Direction.RIGHT);
+		testzone.placeObjectAtEntryPoint(p);
+		assertEquals(0, p.getX());
+		assertEquals(5, p.getY());
+		assertEquals(1, cart.getX());
+		assertEquals(5, cart.getY());
+		
+
+	} //testCart
+} //class
