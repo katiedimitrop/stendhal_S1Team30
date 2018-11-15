b17de347f4d88c9e527e2b172c6cb0bb3f4dd33d
luis zurita <intozlz5@student.manchester.ac.uk>
2018-11-15 10:46:10 +0000

    first wheel barrow test and wheel barrow stub class

---
+++ src/games/stendhal/server/entity/mapstuff/block/Wheelbarrow.java
@@ -0,0 +1,13 @@
+package games.stendhal.server.entity.mapstuff.block;
+
+public class Wheelbarrow extends Block{
+
+	public   int chestposition = 0;
+	public   int position = 0;
+
+	public Wheelbarrow(boolean multipush) {
+		super(multipush);
+	}
+
+
+}
--- tests/games/stendhal/server/entity/mapstuff/block/WheelbarrowTest.java
+++ tests/games/stendhal/server/entity/mapstuff/block/WheelbarrowTest.java
@@ -1,6 +1,6 @@
 package games.stendhal.server.entity.mapstuff.block;
 
-import static org.hamcrest.Matchers.is;
+
 import static org.junit.Assert.*;
 
 import org.junit.BeforeClass;
@@ -16,9 +16,8 @@ import utilities.RPClass.BlockTestHelper;
 
 /**
  * 
- * Test to check if carts dont't refresh when player 
- * leaves zone, and move away from edge of zone if 
- * player enters zone at the positionthe cart is in
+ * Test to check if wheelbarrows keep both parts
+ * together when on map.
  *  
  */
 
@@ -32,39 +31,17 @@ public class WheelbarrowTest {
 	} //beforeClass 
 	
 	@Test
-	public final void testReset() {
-		Wheelbarrow b = new Wheelbarrow(true);
-		b.setPosition(5, 5);
-		b.setResetBlock(false);
+	public final void testTogether() {
+		Wheelbarrow wheelbarrow = new Wheelbarrow(true);
+		wheelbarrow.setPosition(5, 5);;
 		StendhalRPZone z = new StendhalRPZone("test", 10, 10);
-		z.add(b);
+		z.add(wheelbarrow);
 		Player player = PlayerTestHelper.createPlayer("testPlayer");
-		b.push(player, Direction.RIGHT);
-		assertThat(Integer.valueOf(b.getX()), is(Integer.valueOf(6)));
-		assertThat(Integer.valueOf(b.getY()), is(Integer.valueOf(5)));
-		b.onExited(player, z);
-		assertThat(Integer.valueOf(b.getX()), is(Integer.valueOf(6)));
-		assertThat(Integer.valueOf(b.getY()), is(Integer.valueOf(5)));
+		wheelbarrow.push(player, Direction.RIGHT);
+		assertTrue(wheelbarrow.chestposition == wheelbarrow.position+1);
+        fail();
 		
 	} //testReset
 	
-	@Test
-	public final void testWheelbarrowEdge() {
-		Wheelbarrow cart = new Wheelbarrow(true);
-		cart.setPosition(0, 5);
-		StendhalRPZone testzone = new StendhalRPZone("test", 10, 10);
-		testzone.add(cart);
-		Player p = PlayerTestHelper.createPlayer("pusher");
-		assertEquals(0, cart.getX());
-		assertEquals(5, cart.getY());
-		testzone.setEntryPoint(0, 5);;
-		p.setDirection(Direction.RIGHT);
-		testzone.placeObjectAtEntryPoint(p);
-		assertEquals(0, p.getX());
-		assertEquals(5, p.getY());
-		assertEquals(1, cart.getX());
-		assertEquals(5, cart.getY());
-		
 
-	} //testCart
 } //class
